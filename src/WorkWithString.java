f376229570b6c7ed8d56467ce402a3891caa1625
Igor Ignatov <igor.2020.ignatov@mail.ru>
2021-12-25 16:41:35 +0300

    Initial commit

---
+++ src/WorkWithString.java
@@ -0,0 +1,13 @@
+public class WorkWithString {
+
+        public static void main(String[] args) {
+            System.out.println(removeWhiteSpaces("А роза упала на лапу Азора"));
+        }
+
+        private static String removeWhiteSpaces(String str) {
+            //Написать код здесь
+            return str.replace(" ", "");
+
+        }
+
+}
